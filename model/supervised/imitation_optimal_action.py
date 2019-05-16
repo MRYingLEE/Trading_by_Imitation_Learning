1b3476c562f58ab5ce12d77235c4ce51a20c4b28
Ying LI <Mr.Ying.LEE@gmail.com>
2019-05-16 02:08:01 -0400

    Add files via upload

---
+++ model/supervised/imitation_optimal_action.py
@@ -0,0 +1,109 @@
+"""
+Train a classifier given optimal action
+"""
+from utils.data import create_optimal_imitation_dataset, create_imitation_dataset
+
+import numpy as np
+
+from keras.layers import Dense, Activation, BatchNormalization, Dropout, Conv1D, Flatten, MaxPooling1D, Conv2D
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.utils import np_utils
+
+
+def create_network_given_future(nb_classes, winner, weight_path='weights/optimal_3_stocks.h5'):
+    model = Sequential()
+    model.add(Dense(512, input_shape=(nb_classes,)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(lr=1e-3),
+                  metrics=['accuracy'])
+    try:
+        if winner:
+            model.load_weights(weight_path)
+        else:
+            model.load_weights(weight_path+"-loser.h5")
+
+        print('Model load successfully')
+    except:
+        print('Build model from scratch')
+    return model
+
+
+def train_optimal_action_given_future_obs(model, target_history, target_stocks, winner,
+                                          weight_path='weights/optimal_3_stocks.h5'):
+    (X_train, y_train), (X_test, y_test) = create_optimal_imitation_dataset(target_history, winner)
+    nb_classes = len(target_stocks) + 1
+
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+    continue_train = True
+    while continue_train:
+        model.fit(X_train, Y_train, batch_size=128, epochs=20, validation_data=(X_test, Y_test), shuffle=True)
+        save_weights = input('Type True to save weights\n')
+
+        if save_weights:
+            if winner:
+                model.save(weight_path)
+            else:
+                model.save(weight_path+"-loser.h5")
+
+        continue_train = input('True to continue train, otherwise stop\n')
+
+
+def create_network_give_past(nb_classes, window_length, winner, weight_path='weights/imitation_3_stocks.h5'):
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(1, 3), input_shape=(nb_classes, window_length, 1),
+                     activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(filters=32, kernel_size=(1, window_length - 2), input_shape=(nb_classes, window_length - 2, 1),
+                     activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Flatten(input_shape=(window_length, nb_classes)))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(lr=1e-3),
+                  metrics=['accuracy'])
+    try:
+        if winner:
+            model.load_weights(weight_path)
+        else:
+            model.load_weights(weight_path+"-loser.h5")
+        
+        print('Model load successfully')
+    except:
+        print('Build model from scratch')
+    return model
+
+
+def train_optimal_action_given_history_obs(model, target_history, target_stocks, window_length, winner,
+                                           weight_path='weights/imitation_3_stocks.h5'):
+    nb_classes = len(target_stocks) + 1
+    (X_train, y_train), (X_validation, y_validation) = create_imitation_dataset(target_history, window_length,winner=True)
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_validation = np_utils.to_categorical(y_validation, nb_classes)
+    X_train = np.expand_dims(X_train, axis=-1)
+    X_validation = np.expand_dims(X_validation, axis=-1)
+    continue_train = True
+    while continue_train:
+        model.fit(X_train, Y_train, batch_size=128, epochs=100, validation_data=(X_validation, Y_validation),
+                  shuffle=True)
+        save_weights = input('Type True to save weights\n')
+        if save_weights:
+            if winner:
+                model.save(weight_path)
+            else:
+                model.save(weight_path+"-loser.h5")
+
+        continue_train = input("True to continue train, otherwise stop training...\n")
---
+++ model/supervised/lstm.py
@@ -0,0 +1,82 @@
+"""
+Train a supervised CNN model using optimal stock as label
+"""
+import numpy as np
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+from ..base_model import BaseModel
+
+from utils.data import normalize
+
+
+class StockLSTM(BaseModel):
+    def __init__(self, num_classes, window_length, weights_file='weights/lstm.h5'):
+        self.model = None
+        self.weights_file = weights_file
+        self.num_classes = num_classes
+        self.window_length = window_length
+
+    def build_model(self, load_weights=True):
+        """ Load training history from path
+
+        Args:
+            load_weights (Bool): True to resume training from file or just deploying.
+                                 Otherwise, training from scratch.
+
+        Returns:
+
+        """
+        if load_weights:
+            self.model = keras.models.load_model(self.weights_file)
+            print('Successfully loaded model')
+        else:
+            self.model = Sequential()
+            self.model.add(keras.layers.LSTM(20, input_shape=(self.num_classes, self.window_length)))
+            self.model.add(Dense(64))
+            self.model.add(Dropout(0.5))
+            self.model.add(Dense(64))
+            self.model.add(Dropout(0.5))
+            self.model.add(Dense(self.num_classes, activation='softmax'))
+
+            self.model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=1e-4), metrics=['accuracy'])
+            print('Built model from scratch')
+        self.model._make_predict_function()
+        self.graph = tf.get_default_graph()
+
+    def train(self, X_train, Y_train, X_val, Y_val, verbose=True):
+        continue_train = True
+        while continue_train:
+            self.model.fit(X_train, Y_train, batch_size=128, epochs=50, validation_data=(X_val, Y_val),
+                           shuffle=True, verbose=verbose)
+            save_weights = input('Type True to save weights\n')
+            if save_weights:
+                self.model.save(self.weights_file)
+            continue_train = input("True to continue train, otherwise stop training...\n")
+        print('Finish.')
+
+    def evaluate(self, X_test, Y_test, verbose=False):
+        return self.model.evaluate(X_test, Y_test, verbose=verbose)
+
+    def predict(self, X_test, verbose=False):
+        return self.model.predict(X_test, verbose=verbose)
+
+    def predict_single(self, observation):
+        """ Predict the action of a single observation
+
+        Args:
+            observation: (num_stocks + 1, window_length)
+
+        Returns: a single action array with shape (num_stocks + 1,)
+
+        """
+        action = np.zeros((self.num_classes,))
+        obsX = observation[:, -self.window_length:, 3] / observation[:, -self.window_length:, 0]
+        obsX = normalize(obsX)
+        obsX = np.expand_dims(obsX, axis=0)
+        with self.graph.as_default():
+            current_action_index = self.model.predict_classes(obsX, verbose=False)
+        action[current_action_index] = 1.0
+        return action
